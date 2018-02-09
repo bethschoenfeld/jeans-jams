c4dc5958759eabf25398a6e3ba71c3c322bff4bc
bethschoenfeld <bethingraham3@gmail.com>
2018-02-09 12:20:55 -0500

    got all the routes working in the contoroller.

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,4 +1,40 @@
 class Api::ArtistsController < ApplicationController
+  def index
+    @artist = Artist.all
+    render json: @artist
+  end
 
+  def show
+      @artist = Artist.find(params[:id])
+      render json: @artist
+  end
+
+  def create
+    @artist = Artist.create!(artist_params)
+    render json: @artist
+  end
+
+  def update
+    @artist = Artist.find(params[:id])
+    @artist + update!(artist_params)
+
+    render json: @artist
+  end
+
+  def destroy
+    @artist = Artist.find(params[:id]).delete
+
+  end
+
+  private
+  def artist_params
+    params
+    .require(:artist)
+    .permit(
+      :name, 
+      :nationality,
+      :photo_url
+      )
+  end
 
 end
